d14d8d517609f91db9903b13ed46af0b5e09c445
Liam <liam.franssen@stud-mail.uni-wuerzburg.de>
2020-09-18 11:54:23 +0200

    Run Script/Function added
    Function not complete yet

--- modules/telestion-core/src/main/java/org/telestion/core/matlabconnector/Connector.java
+++ modules/telestion-core/src/main/java/org/telestion/core/matlabconnector/Connector.java
@@ -11,25 +11,46 @@ import java.util.Scanner;
 
 public class Connector {
 
-
+    Scanner sg=new Scanner(System.in);
     //public static void main(String[] args) {
     //}
+    public Connector(){
+
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner s=new Scanner(System.in);
         //Future<MatlabEngine> engFuture = MatlabEngine.startMatlabAsync();
         //MatlabEngine eng = engFuture.get();
         MatlabEngine eng = MatlabEngine.startMatlab();
-        eng.eval("RunRungeKuttaSimulationWind");
-        System.out.println("want to close");
+        //eng.eval("RunRungeKuttaSimulationWind");
+
+        System.out.println("want to close Matlab");
         String k=s.next();
         if (k.equals("no") ||k.equals("n")) {
-
-        }else {
             eng.close();
+        }else {
+
         }
     }
 
-    public void rungwind(){
+
+    public void rungwind(MatlabEngine engine) throws Exception{
+        engine.eval("RunRungeKuttaSimulationWind");
+
+    }
+
+    public void runAnyMatlabFunc(MatlabEngine engine,String fun) throws Exception{
+
+        engine.feval(fun);
+
+    }
+    public void runAnyMatlabScript(MatlabEngine engine,String script) throws Exception{
+        engine.eval(script);
+
+    }
+    public void runAnyMatlabFc(MatlabEngine engine,String fun) throws Exception{
+        engine.eval("RunRungeKuttaSimulationWind");
 
     }
 }
